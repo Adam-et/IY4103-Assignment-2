97bdf0a53fd9c510de32c216ba51e3117254a4d8
Adam-et <adamtarabishy19@gmail.com>
2025-04-29 17:19:31 +0100

    Part2

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#Routing
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -33,6 +41,20 @@ def manga_detail():
 @app.route("/checkout")
 def checkout():
     return render_template("checkout.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+# Database Connection
+
+
+
+#Database Objects
 
 if __name__ == '__main__':
     app.run(debug = True)
